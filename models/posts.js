522f1967ca30b905f9e4e3c911dccb17e777fc42
micklethepickle <michel_ma@hotmail.ca>
2017-02-04 21:52:18 -0500

    changed everything to string

--- models/posts.js
+++ models/posts.js
@@ -3,8 +3,8 @@ var mongoose = restful.mongoose;
 
 //schema
 var postsSchema = new mongoose.Schema({
-	busNumber: Number,
-	lateness: Number, // 0 on time, 1 early, -1 late
+	busNumber: String,
+	lateness: String, // 0 on time, 1 early, -1 late
 	location: String,
 	currentTime: String
 })
