905c048730dffcea7862a1eed2fd0dcc72cac970
micklethepickle <michel_ma@hotmail.ca>
2017-02-04 20:34:07 -0500

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 //mongo
 mongoose.connect('mongodb://localhost/rest_test');
 
 //express
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
